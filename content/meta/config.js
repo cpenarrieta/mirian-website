13c8cf984f1209bf5fd2975946f81aeb0e17789e
Cristian Peñarrieta <cristian.penarrieta@gmail.com>
2018-09-11 00:45:20 -0700

    initial commit

---
+++ content/meta/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteTitle: "Mirian Oria", // <title>
+  shortSiteTitle: "Mirian Oria Blog", // <title> ending for posts and pages
+  siteDescription: "Blog de Mirian Oria",
+  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "es",
+  // author
+  authorName: "Cristian Penarrieta",
+  authorTwitterAccount: "cpenarrieta",
+  // info
+  headerTitle: "Mirian Oria",
+  headerSubTitle: "Hola soy Mirian Oria",
+  // manifest.json
+  manifestName: "Mirian Oria Blog",
+  manifestShortName: "MirianOria", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/cpenarrieta" },
+    { name: "twitter", url: "https://twitter.com/cpenarrieta" },
+    { name: "facebook", url: "http://facebook.com/cristian.penarrieta" }
+  ]
+};
